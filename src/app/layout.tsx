0525539ac2439ce7442aea46c51cc758eff4a74e
SaneryiMatt <22301135@bjtu.edu.cn>
2026-03-16 23:14:08 +0800

    feat: add @vercel/analytics and @vercel/speed-insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 ﻿import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import "@/app/globals.css";
 import { cn } from "@/lib/utils";
